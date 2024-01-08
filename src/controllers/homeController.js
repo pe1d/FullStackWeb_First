8224990f4bb5af60d4b9828a471945f8bcb97692
Xuan Diep <xuandiep003@gmail.com>
2024-01-08 21:01:17 +0700

    #21

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,17 @@
-let getHomePage = (req, res) => {
-    return res.render("homePage.ejs");
+import db from "../models";
+
+
+
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        console.log('--------------', data)
+        return res.render("homePage.ejs", {
+            data: JSON.stringify(data)
+        });
+    } catch (e) {
+        console.log(e)
+    }
 }
 let getAboutPage = (req, res) => {
     return res.render("test/about.ejs");
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import viewEngine from './config/viewEngine'
 import initWebRoutes from './route/web'
+import connectDB from './config/connnectDB'
 require('dotenv').config();
 
 let app = express();
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 viewEngine(app);
 initWebRoutes(app);
 
+connectDB();
 let port = process.env.PORT || 6969;
 app.listen(port, () => {
     //callback
